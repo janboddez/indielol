da078c7b102ad8f903e20a3143e1334d44ed3088
Jan Boddez <jan.boddez@gmail.com>
2023-08-12 17:25:36 +0200

    Various fixes

--- patterns/hidden-no-results.php
+++ patterns/hidden-no-results.php
@@ -10,4 +10,4 @@
 <p><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'indielol' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'indielol' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'indielol' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'indielol' ); ?>","buttonUseIcon":true} /-->
+<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'indielol' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'indielol' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'indielol' ); ?>","buttonUseIcon":true,"style":{"border":{"radius":"4px","width":"1px"}},"borderColor":"tertiary"} /-->
