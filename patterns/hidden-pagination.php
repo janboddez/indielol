3a63acc524925d029fc9697df768609975b6f85c
Jan Boddez <jan.boddez@gmail.com>
2023-08-14 22:50:50 +0200

    Fix #3

---
+++ patterns/hidden-pagination.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Hidden Pagination
+ * Slug: indielol/hidden-pagination
+ * Inserter: no
+ */
+?>
+
+<!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<!-- wp:query-pagination-previous {"label":"<?php echo esc_html_x( 'Newer Posts', 'label', 'indielol' ); ?>"} /-->
+
+	<!-- wp:query-pagination-next {"label":"<?php echo esc_html_x( 'Older Posts', 'label', 'indielol' ); ?>"} /-->
+<!-- /wp:query-pagination -->
